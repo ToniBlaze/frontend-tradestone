f681adc0028c213f34e8094be8b35ca55d07d542
Alessio <alessio.toninello@gmail.com>
2024-07-07 16:01:27 +0200

    adding style homepage

--- src/pages/Homepage.jsx
+++ src/pages/Homepage.jsx
@@ -1,11 +1,27 @@
 import React from "react";
+import { useNavigate } from "react-router-dom";
 import { Container, Row } from "react-bootstrap";
+import "./homepage.css";
 
 export default function Homepage() {
+  const navigate = useNavigate();
+
+  const onClickLogin = () => {
+    navigate("/login");
+    window.scrollTo(0, 0);
+  };
+
+  const onClickRegister = () => {
+    navigate("/register");
+    window.scrollTo(0, 0);
+  };
+
+
   return (
-    <Container id="home" fluid>
+    <Container id="home" className="home-bg" fluid>
       <Row>
-        <h1> HOME PAGE </h1>
+        <button className="home-btn col-12 btn-mt" onClick={onClickLogin}>Login</button>
+        <button className="home-btn col-12" onClick={onClickRegister}>Register</button>
       </Row>
     </Container>
   );
